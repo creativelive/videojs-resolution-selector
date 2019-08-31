6665bb3b040be3e230cf8d8109cf153cf7521b50
Roger Gonzalez <rg@creativelive.com>
2019-08-31 14:32:25 -0700

    build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-for stage in .build/*; do
-  source "${stage}"
+for stage in .build/*.sh; do
+  echo "==== ${stage} ===="
+  (. "${stage}")
+  code=$?
+  if [[ $code -ne 0 ]]; then
+    echo "*** exiting with code $code ***"
+    exit $code
+  fi
 done
+exit 0
+
